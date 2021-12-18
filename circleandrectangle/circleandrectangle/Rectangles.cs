791a80ba15d0f7e7d30e6c1726256805cf7175da
EmiliaBel <beea.20@uni-dubna.ru>
2021-12-18 11:40:30 +0300

    добавлены файлы по рисованию

---
+++ circleandrectangle/circleandrectangle/Rectangles.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace circleandrectangle
+{
+    class Rectangles: Figure
+    {
+        Pen pen;
+        int height, width;
+
+        public Rectangles(int x, int y, int height, int width, Pen pen)
+        {
+            this.pen = pen;
+            this.x = x;
+            this.y = y;
+            this.height = height;
+            this.width = width;
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            gr.DrawRectangle(pen, x, y, width, height);
+        }
+
+        //тут тоже что-то не то с x и y, типо почему-то он проходит через конструктор, не знаю правильно это или нет и что с этим делать, типо внешне оно работает, но с кодом какая-то шняга
+        //или норм всё, не понимаю
+
+        public override bool IsPointInside(int x, int y)
+        {
+            if ((this.x + width >= x) && (this.y + height >= y) && (this.x <= x) && (this.y <= y))
+                return true;
+            else return false;
+        }
+
+        public override void Move(int x, int y)
+        {
+            this.x += x;
+            this.y += y;
+        }
+    }
+}
---
+++ circleandrectangle/circleandrectangle/WagonSand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circleandrectangle
+{
+    class WagonSand: Figure
+    { 
+        Circle c, c2;
+        Rectangles rect;
+        Point[] points;
+        int height, width;
+        Brush brush = new SolidBrush(Color.Gold);
+        public WagonSand(int height, int width, int x, int y, Pen pen, Pen penR)
+        {
+            this.height = height;
+            this.width = width;
+            rect = new Rectangles(x, y, height, width, pen);
+            c = new Circle(penR, x + Convert.ToInt32(0.2 * width), y + Convert.ToInt32(height + Convert.ToInt32(0.07 * width)), Convert.ToInt32(0.07 * width));
+            c2 = new Circle(penR, x + Convert.ToInt32(0.8 * width), y + Convert.ToInt32(height + Convert.ToInt32(0.07 * width)), Convert.ToInt32(0.07 * width));
+            Point point1 = new Point(x, y);
+            Point point2 = new Point(x + width, y);
+            Point point3 = new Point(x + Convert.ToInt32(0.5 * width), y - Convert.ToInt32(0.5 * height));
+            points = new Point[]{ point1, point2, point3 };
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            rect.Draw(gr);
+            c.Draw(gr);
+            c2.Draw(gr);
+            gr.FillPolygon(brush, points);
+        }
+
+        public override bool IsPointInside(int x, int y)
+        {
+            if (c.IsPointInside(x, y) || c2.IsPointInside(x, y) || rect.IsPointInside(x, y))
+                return true;
+            else return false;
+        }
+
+        public override void Move(int x, int y)
+        {
+            c.Move(x, y);
+            c2.Move(x, y);
+            rect.Move(x, y);
+            for (int i = 0; i < 3; i++)
+            {
+                points[i].X += x;
+                points[i].Y += y;
+            }
+        }
+
+    }
+}
+
